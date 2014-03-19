984cb924876a0dcda7116b6f4046d690640ada58
BarthS <barthelemy.saugrain@gmail.com>
2014-03-19 18:01:08 +0100

    page acceuil root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
+
 LBC::Application.routes.draw do
+  get "accueil/index"
+  root 'accueil#index'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
