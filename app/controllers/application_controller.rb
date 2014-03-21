70192d0f1c7224092fca2d89f9a7ff62e0099a6e
Barthelemy Saugrain <Bart@mba-bart.home>
2014-03-21 08:46:32 +0100

    correct bug current pw

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
    def configure_permitted_parameters
      devise_parameter_sanitizer.for(:sign_in){ |u| u.permit(:email, :password) }
      devise_parameter_sanitizer.for(:sign_up){ |u| u.permit(:nom, :prenom, :promo,  :email, :password, :password_confirmation)}
-     devise_parameter_sanitizer.for(:account_update){ |u| u.permit(:nom, :prenom, :promo, :email, :password, :password_confirmation) }
+     devise_parameter_sanitizer.for(:account_update){ |u| u.permit(:nom, :prenom, :promo, :email, :password, :password_confirmation, :current_password) }
    end
 
 end
