5cdd047ba36931cfe45d0caadd54f3d46cd4f118
BarthS <barthelemy.saugrain@gmail.com>
2014-03-27 20:00:21 +0100

    memberlist

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 
 LBC::Application.routes.draw do
+  get "users/index"
+  get "users/show"
+  get "users/edit"
+  get "users/update"
+  get "users/destroy"
   resources :annonces
   devise_for :users
   get "accueil/index"
